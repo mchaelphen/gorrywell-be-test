6de29581402e26e06b4c01c8305824eae049e5f0
mchaelphen <mchaelphen@gmail.com>
2023-03-30 21:49:08 +0700

    clean code

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ import { checkJwt, isMentor } from "../middlewares/checkJwt";
 const router = Router();
 
 router.get("/", UserController.getAll);
-router.get("/get-mentee/:id", [checkJwt], [isMentor], UserController.getMentee);
+router.get("/get-mentee/:id", [checkJwt, isMentor], UserController.getMentee);
 router.get("/get-leaderboard-per-class/:id", [checkJwt], UserController.getLeaderboardByClassroom);
 router.patch("/assign-mentee/:id", [checkJwt, isMentor], UserController.assignMentee);
 router.patch("/update-score/:id", [checkJwt, isMentor], UserController.updateScore);
